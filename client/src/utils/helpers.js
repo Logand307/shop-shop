e58019ef58282017bc54c6eb9945da6560f65785
Logan Davis <Ldavis96@live.com>
2022-04-30 15:35:34 -0400

    started helper function for idb

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,6 +1,10 @@
 export function pluralize(name, count) {
   if (count === 1) {
-    return name
+    return name;
   }
-  return name + 's'
+  return name + "s";
+}
+
+export function idbPromise(storeName, method, object) {
+  return new Promise((resolve, reject) => {});
 }
