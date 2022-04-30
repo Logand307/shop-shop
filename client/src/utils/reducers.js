62dda6e39ea1770eb26c90bd7c29654b8281dc89
Logan Davis <Ldavis96@live.com>
2022-04-30 14:45:24 -0400

    toggle_cart test passing

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -73,6 +73,12 @@ export const reducer = (state, action) => {
         cart: [],
       };
 
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
+
     default:
       return state;
   }
