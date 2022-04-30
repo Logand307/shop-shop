c46fbc535248d721e87a123561ff183e292b5941
Logan Davis <Ldavis96@live.com>
2022-04-30 15:20:26 -0400

    removeFromCart functioning

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -50,6 +50,13 @@ function Detail() {
     }
   };
 
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id,
+    });
+  };
+
   return (
     <>
       {currentProduct ? (
@@ -63,7 +70,12 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{" "}
             <button onClick={addToCart}>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button
+              disabled={!cart.find((p) => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+            </button>
           </p>
 
           <img
