a4453163f12d3d656018e30ff53eb45c5d8ac29d
Logan Davis <Ldavis96@live.com>
2022-04-30 14:43:19 -0400

    remove_from_cart amd update_cart_quantity tests passing

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -43,6 +43,29 @@ export const reducer = (state, action) => {
         cart: [...state.cart, ...action.products],
       };
 
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+
     default:
       return state;
   }
