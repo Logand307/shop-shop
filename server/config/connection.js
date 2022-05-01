87d78ab40bcbe89a5b6ea3fc7a535a4f80a7bde8
Logan Davis <Ldavis96@live.com>
2022-04-30 22:02:53 -0400

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://Logand307:lDAV0929$@cluster0.t01pq.mongodb.net/SHOP-SHOP?retryWrites=true&w=majority",
+  process.env.MONGODB_URI || "mongodb://localhost/mernshopping",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   }
 );
 
